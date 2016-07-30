9d84f8c9d5e20c8a513fb0d4479af676aeb00ea1
Ben McCormick <ben.mccormick@windsorcircle.com>
2016-07-29 23:35:14 -0400

    Clean up main.js and only request a token when we don't have one

--- main.js
+++ main.js
@@ -1,55 +1,62 @@
 const electron = require('electron');
 const path = require('path');
-const secrets = require('./config/secrets');
 // Module to control application life.
 const {app, ipcMain} = electron;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow, authWindow;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {
-      nodeIntegration: true,
-      preload: path.join(__dirname, '/src/indexpreload.js'),
-  }});
+const createWindow = () => {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, '/src/indexpreload.js'),
+        }
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html')
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+};
 
-function createAuthWindow () {
-  // Create the browser window.
-  authWindow = new BrowserWindow({width: 10, height: 10, webPreferences: {
-      nodeIntegration: false,
-      preload: path.join(__dirname, '/src/authpreload.js'),
-  }});
+const createAuthWindow = () => {
+    // Create the browser window.
+    authWindow = new BrowserWindow({
+        width: 10,
+        height: 10,
+        webPreferences: {
+            nodeIntegration: false,
+            preload: path.join(__dirname, '/src/authpreload.js'),
+        }
+    });
 
-  // and load the index.html of the app.
-  authWindow.loadURL('file://' + __dirname + '/auth.html')
+    // and load the index.html of the app.
+    authWindow.loadURL('file://' + __dirname + '/auth.html');
 
-  // Emitted when the window is closed.
-  authWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    authWindow = null
-  })
-}
+    // Emitted when the window is closed.
+    authWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        authWindow = null;
+    });
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -59,25 +66,26 @@ app.on('ready', () => {
 });
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
 let sendTokenBack = () => null;
 ipcMain.on('get-token', (event, token) => {
     createAuthWindow();
-    sendTokenBack = (token) => event.sender.send('set-token', token);
+    sendTokenBack = t => event.sender.send('set-token', t);
 });
 
 ipcMain.on('set-token', (event, token) => {
@@ -85,6 +93,3 @@ ipcMain.on('set-token', (event, token) => {
     //send along to the main process
     sendTokenBack(token);
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- src/setup/trello_client.js
+++ src/setup/trello_client.js
@@ -2,14 +2,28 @@
 import Trello from 'troll-client';
 const {ipcRenderer} = require('electron');
 
+const useToken = (token: string, trelloClient: any, resolve: any) => {
+    trelloClient.setToken(token);
+    resolve(token);
+};
+
+const getToken = (trelloClient: any, resolve: any) => {
+    ipcRenderer.send('get-token');
+    ipcRenderer.on('set-token', (event, token) => {
+        localStorage.setItem('trelloToken', token);
+        useToken(token);
+    });
+};
+
 export const getTrelloClient = () => {
     return new Promise((resolve, reject) => {
         let t = new Trello(window.trelloKey);
-        ipcRenderer.send('get-token');
-        ipcRenderer.on('set-token', (event, token) => {
-            t.setToken(token);
-            resolve(t);
-        });
+        let token = localStorage.getItem('trelloToken');
+        if (token) {
+            useToken(token, t, resolve);
+        } else {
+            getToken(t, resolve);
+        }
 
     });
 };
